0da4369343cb0d815c32fafe77d3e8531277b325
tapio sirvio <tapio.sirvio11@gmail.com>
2018-09-26 12:48:14 +0300

    created 1st solution

---
+++ conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                // prompt user
+                Console.Write("please enter 3 numbers");
+                Console.WriteLine();
+
+                // Read user input
+                string userInput;
+                userInput = Console.ReadLine();
+                string userInput1;
+                userInput1 = Console.ReadLine();
+                string userInput2;
+                userInput2 = Console.ReadLine();
+
+                // Evaluate user input
+
+                int evaluatedNumber;
+                bool isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+                int evaluatedNumber1;
+                int.TryParse(userInput1, out evaluatedNumber1);
+
+                int evaluatedNumber2;
+                int.TryParse(userInput2, out evaluatedNumber2);
+
+                if (evaluatedNumber < evaluatedNumber1)
+                {
+                    if (evaluatedNumber < evaluatedNumber2)
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber, evaluatedNumber1, evaluatedNumber2);
+                    }
+                    else if (evaluatedNumber1 < evaluatedNumber2)
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber, evaluatedNumber2, evaluatedNumber1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber2, evaluatedNumber, evaluatedNumber1);
+                    }
+                }
+
+                else
+                {
+                    if (evaluatedNumber < evaluatedNumber2)
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber1, evaluatedNumber, evaluatedNumber2);
+                    }
+                    else if (evaluatedNumber1 < evaluatedNumber2)
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber1, evaluatedNumber2, evaluatedNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0},{1},{2}", evaluatedNumber2, evaluatedNumber1, evaluatedNumber);
+                    }
+                }
+
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
