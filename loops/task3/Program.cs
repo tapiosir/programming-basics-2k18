7d75a958e9a383d99d321f0b01cef8bff452539b
tapio sirvio <tapio.sirvio11@gmail.com>
2018-10-07 17:24:59 +0300

    submitting task3

--- loops/task3/Program.cs
+++ loops/task3/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace task3
+namespace task03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("enter a number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            bool isTrue = int.TryParse(userInput, out int num01);
+            int eSum = 0;
+            int oSum = 0;
+            if (isTrue) { 
+            for (int i = 1; i <= num01; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    eSum = eSum + i;
+                }
+
+                else
+                {
+                    oSum = oSum + i;
+                }
+
+
+            }
+            Console.WriteLine($"even numbers sum {eSum}");
+            Console.WriteLine($"odd numbers sum {oSum}");
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
         }
     }
 }
