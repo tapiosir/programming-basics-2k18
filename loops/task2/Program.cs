8283b304750fcc11ce08d172f15b42b4df0cbffc
tapio sirvio <tapio.sirvio11@gmail.com>
2018-10-07 17:09:33 +0300

    comitting task2

--- loops/task2/Program.cs
+++ loops/task2/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace task2
+namespace task02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("enter a positive number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+           bool isTrue = int.TryParse(userInput, out  int num01);
+            int i = 1;
+            int j = 1;
+            if (isTrue) { 
+            if (num01 < 1)
+            {
+                Console.WriteLine("number too small");
+            }
+
+            else
+            {
+                while (i++ < num01)
+                {
+                    j = j + i;
+                }
+                Console.WriteLine($"{j}");
+            }
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
         }
     }
 }
