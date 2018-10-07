79168f8bdd874b96da19733624e62ab6a7c7f8e7
tapio sirvio <tapio.sirvio11@gmail.com>
2018-10-07 17:52:19 +0300

    committing tasks 4 and 5 also made code little prettier in tasks 1,2 and 3

--- loops/task4/Program.cs
+++ loops/task4/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace task4
+namespace task04
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("enter a number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            
+            bool isTrue = int.TryParse(userInput, out int num01);
+            int i = 1;
+            int j = 1;
+            int k = -1;
+            int l = -1;
+            if (isTrue) { 
+            if (num01 < 1)
+            {
+                while (l-- > num01)
+                {
+                    k = k + l;
+                }
+                Console.WriteLine($"{k}");
+
+            }
+
+            else
+            {
+                while (i++ < num01)
+                {
+                    j = j + i;
+                }
+                Console.WriteLine($"{j}");
+            }
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
+
         }
     }
 }
--- loops/task5/Program.cs
+++ loops/task5/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 
-namespace task5
+namespace task05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("enter a number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            
+            bool isTrue = int.TryParse(userInput, out int num01);
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            int m = 0;
+            if (isTrue) { 
+            if (num01 < 0)
+            {
+                for (int n = -1; n >= num01; n--)
+                {
+                    if (n % 2 == 0)
+                    {
+                        l = l + n;
+                    }
+                    else
+                    {
+                        m = m + n;
+                    }
+                }
+                Console.WriteLine($"even numbers sum {l}");
+                Console.WriteLine($"odd numbers sum {m}");
+            }
+            else
+            {
+                for (int i = 1; i <= num01; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        j = j + i;
+                    }
+                    else
+                    {
+                        k = k + i;
+                    }
+                }
+                Console.WriteLine($"even numbers sum {j}");
+                Console.WriteLine($"odd numbers sum {k}");
+            }
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
         }
     }
 }
